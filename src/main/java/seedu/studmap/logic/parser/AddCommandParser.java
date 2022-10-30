d3b79f8c6529181dff3cf63b7e94725579a78708
dr-arrgghh <dr.arrgghh@gmail.com>
2022-10-30 23:44:14 +0800

    Fix Add Command Bugs

--- src/main/java/seedu/studmap/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/studmap/logic/parser/AddCommandParser.java
@@ -9,6 +9,23 @@ import static seedu.studmap.logic.parser.CliSyntax.PREFIX_MODULE;
 import static seedu.studmap.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.studmap.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.studmap.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_B;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_D;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_F;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_G;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_I;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_J;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_K;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_L;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_O;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_Q;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_R;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_U;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_V;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_W;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_X;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_Y;
+import static seedu.studmap.logic.parser.InvalidCliSyntax.Invalid_Prefix_Z;
 
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -31,6 +48,10 @@ public class AddCommandParser implements Parser<AddCommand> {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
+    private static boolean containInvalidPrefix(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
     /**
      * Parses the given {@code String} of arguments in the context of the AddCommand
      * and returns an AddCommand object for execution.
@@ -41,9 +62,19 @@ public class AddCommandParser implements Parser<AddCommand> {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_MODULE,
                         PREFIX_ID, PREFIX_GIT, PREFIX_HANDLE, PREFIX_TAG);
+        ArgumentMultimap invalidMultiMap =
+                ArgumentTokenizer.tokenize(args,Invalid_Prefix_B, Invalid_Prefix_D, Invalid_Prefix_F, Invalid_Prefix_G,
+                        Invalid_Prefix_I, Invalid_Prefix_J, Invalid_Prefix_K, Invalid_Prefix_L, Invalid_Prefix_O,
+                        Invalid_Prefix_Q, Invalid_Prefix_R, Invalid_Prefix_U, Invalid_Prefix_V, Invalid_Prefix_W,
+                        Invalid_Prefix_X, Invalid_Prefix_Y, Invalid_Prefix_Z);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_MODULE, PREFIX_ID)
                 || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        } else if (containInvalidPrefix(invalidMultiMap, Invalid_Prefix_B, Invalid_Prefix_D, Invalid_Prefix_F, Invalid_Prefix_G,
+                Invalid_Prefix_I, Invalid_Prefix_J, Invalid_Prefix_K, Invalid_Prefix_L, Invalid_Prefix_O,
+                Invalid_Prefix_Q, Invalid_Prefix_R, Invalid_Prefix_U, Invalid_Prefix_V, Invalid_Prefix_W,
+                Invalid_Prefix_X, Invalid_Prefix_Y, Invalid_Prefix_Z)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
 
---
+++ src/main/java/seedu/studmap/logic/parser/InvalidCliSyntax.java
@@ -0,0 +1,21 @@
+package seedu.studmap.logic.parser;
+
+public class InvalidCliSyntax {
+    public static final Prefix Invalid_Prefix_B = new Prefix("b/");
+    public static final Prefix Invalid_Prefix_D = new Prefix("d/");
+    public static final Prefix Invalid_Prefix_F = new Prefix("f/");
+    public static final Prefix Invalid_Prefix_G = new Prefix("g/");
+    public static final Prefix Invalid_Prefix_I = new Prefix("i/");
+    public static final Prefix Invalid_Prefix_J = new Prefix("j/");
+    public static final Prefix Invalid_Prefix_K = new Prefix("k/");
+    public static final Prefix Invalid_Prefix_L = new Prefix("l/");
+    public static final Prefix Invalid_Prefix_O = new Prefix("o/");
+    public static final Prefix Invalid_Prefix_Q = new Prefix("q/");
+    public static final Prefix Invalid_Prefix_R = new Prefix("r/");
+    public static final Prefix Invalid_Prefix_U = new Prefix("u/");
+    public static final Prefix Invalid_Prefix_V = new Prefix("v/");
+    public static final Prefix Invalid_Prefix_W = new Prefix("w/");
+    public static final Prefix Invalid_Prefix_X = new Prefix("x/");
+    public static final Prefix Invalid_Prefix_Y = new Prefix("y/");
+    public static final Prefix Invalid_Prefix_Z = new Prefix("z/");
+}
