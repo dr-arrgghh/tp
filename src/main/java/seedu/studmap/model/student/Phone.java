cb69e48db4b88189c2f0ae7cb95e449847d1dae9
dr-arrgghh <dr.arrgghh@gmail.com>
2022-10-30 23:56:52 +0800

    Update Phone.java

--- src/main/java/seedu/studmap/model/student/Phone.java
+++ src/main/java/seedu/studmap/model/student/Phone.java
@@ -12,8 +12,9 @@ public class Phone {
     public static final String NO_PHONE_STRING = "No Phone number";
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
-    public static final String VALIDATION_REGEX = "\\d{3,}";
+            "Phone numbers should only contain numbers, " +
+                    " start with 8 or 9 and it should have a length of 8";
+    public static final String VALIDATION_REGEX = "^[8-9]\\d{7}";
     public final String value;
 
     /**
